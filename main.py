2cb089e7fa4a618c8d3cdfe1dcb2ed7928bd992c
GoodGuyGregory <greg.witt625@gmail.com>
2024-02-19 00:18:00 -0500

    adds data filtering for training and test sets

--- main.py
+++ main.py
@@ -4,30 +4,59 @@ import pandas as pd
 
 # build 2300 instances of spam and ham
 # split 40% spam 60% ham
-def buildTrainingData():
-    training_spambase = pd.read_csv("spambase.data")
-    spamPulls = 2300 * .40
-    hamPulls = 2300 * .60
-    pullSpams(spamPulls, training_spambase)
-    pullHams(hamPulls, training_spambase)
+def buildTrainingTestData(spamSplit, hamSplit):
+    spambase = pd.read_csv("spambase.data")
 
-    # shuffle the data.
+    # get the number of spams and hams required
+    spamPulls = int(2300 * spamSplit)
+    hamPulls = int(2300 * hamSplit)
 
-
-def buildTestingData():
-
-def pullHams(size, data):
-    # finds the size specified but pulls hams
-
-    # randomly pull the specified size of non-spam emails
+    #  shuffle original data
+    spambase = spambase.sample(frac=1)
 
 
-def pullSpams(size, data):
-    # finds the size specified but pulls spams
+    # Training Data Preparations:
+    # =======================================================
+
+    #  pull only the desired number of spams
+    trainingSpams = spambase[spambase.iloc[:, -1] == 1][:spamPulls]
+    trainingHams = spambase[spambase.iloc[:,-1] == 0][:hamPulls]
+
+    # removes data so test can't pull it into its own DF
+    #  this is done with a boolean mask to remove the corresponding index values from the original df
+    spambase = spambase[~spambase.index.isin(trainingSpams.index)]
+    spambase = spambase[~spambase.index.isin(trainingHams.index)]
+
+
+    training_spambase = pd.concat([trainingHams, trainingSpams])
+
+    #  Testing Data
+    # ==============================================================
+
+    #  pull only the desired number of spams
+    testingSpams = spambase[spambase.iloc[:, -1] == 1][:spamPulls]
+    testingHams = spambase[spambase.iloc[:, -1] == 0][:hamPulls]
+
+    # removes data so test can't pull it into its own DF
+    # ~ negates the boolean mask to select the rows that are not present in spambase and filter them
+    spambase = spambase[~spambase.index.isin(testingSpams.index)]
+    spambase = spambase[~spambase.index.isin(testingHams.index)]
+
+    testing_spambase = pd.concat([testingHams, testingSpams])
+    # return both dataframes
+    trainingTestingDfs = []
+
+    trainingTestingDfs.append(training_spambase)
+    trainingTestingDfs.append(testing_spambase)
+
+    # return the data from split
+    return trainingTestingDfs
+
 
 
 def main():
 
+    trainingTestingDataList = buildTrainingTestData(.40,.60)
 
 
 main()
