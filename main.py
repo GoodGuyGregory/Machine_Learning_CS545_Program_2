b9a05885f18a31395fc99a71e33395ca3d854670
GoodGuyGregory <greg.witt625@gmail.com>
2024-02-18 19:53:01 -0500

    adds intial split logic in buildTrainingData

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
+
+
+# build 2300 instances of spam and ham
+# split 40% spam 60% ham
+def buildTrainingData():
+    training_spambase = pd.read_csv("spambase.data")
+    spamPulls = 2300 * .40
+    hamPulls = 2300 * .60
+    pullSpams(spamPulls, training_spambase)
+    pullHams(hamPulls, training_spambase)
+
+    # shuffle the data.
+
+
+def buildTestingData():
+
+def pullHams(size, data):
+    # finds the size specified but pulls hams
+
+    # randomly pull the specified size of non-spam emails
+
+
+def pullSpams(size, data):
+    # finds the size specified but pulls spams
+
 
 def main():
 
